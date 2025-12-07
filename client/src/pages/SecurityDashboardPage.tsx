0948c6aa8d53db6d3530fc1db3f1c681187e3929
YOYO1069 <190172450+YOYO1069@users.noreply.github.com>
2025-12-07 00:03:57 -0500

    修正 SecurityDashboardPage 的 import 路徑

--- client/src/pages/SecurityDashboardPage.tsx
+++ client/src/pages/SecurityDashboardPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'wouter';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../lib/supabase.ts';
 import { getRiskLevelColor, getRiskLevelIcon, getRiskLevelText, getFlagDescription } from '../utils/riskScoring';
 
 /**
